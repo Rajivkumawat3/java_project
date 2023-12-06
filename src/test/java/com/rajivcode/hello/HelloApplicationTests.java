00f56531eac9fb19fd8896c41c4038956fbfc20c
Rajiv Kumawat <rajivkumawat28012002@gmail.com>
2023-12-06 12:57:18 +0530

    first commit

---
+++ src/test/java/com/rajivcode/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajivcode.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
